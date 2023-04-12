f39f089f3f825ae4f2e7687897aa57a055042d54
hiteshdci <hitesh@itexpertsindya.com>
2023-04-12 19:02:26 +0530

    delete

--- app/controllers/accessRestrictedController.js
+++
@@ -1,3 +0,0 @@
-app.controller('accessRestrictedController', function(Page,$scope,$http){
-	Page.setTitle('Access Restricted');	
-});
--- app/controllers/jobsearch.js
+++
@@ -1,8 +0,0 @@
-//define jobsearch 
-app.controller('jobsearch', function(Page,$scope,$timeout,$http,nukeService) {
-    
-    Page.setTitle("Job Search");
-    $scope.site_url = global_base_url+'job-search-script';
-    
-
-});
